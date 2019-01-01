01f74731eac0489480db80bca69300009311ce9c
Thanh Van <thanh.van@smonv.io>
2019-01-01 11:38:46 +0700

    remove build dir

--- dist/graphql.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class City {
-}
-exports.City = City;
-class IQuery {
-}
-exports.IQuery = IQuery;
-//# sourceMappingURL=graphql.js.map
